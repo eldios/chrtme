5e54113be0963ae3e47dbca3f0fc6b98c1bb40a9
Emanuele 'Lele' Calo <lele@quasinormale.it>
2016-05-15 04:00:17 +0200

    Continued work on refactoring

---
+++ chrtme/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+
+    "version_info", "__version__",
+]
+
+__author__ = "Emanuele 'Lele' Calo'"
+__version__ = "0.0.5"
+version_info = tuple([int(num) for num in __version__.split('')])
