19610d21a9218131e71b09ad01730a365c43c5f4
Emanuele 'Lele' Calo <lele@quasinormale.it>
2016-05-25 15:18:54 +0200

    More work on refactoring and adding tests

--- chrtme/chrtme.py
+++ chrtme/chrtme.py
@@ -1,7 +1,50 @@
 class Chrtme:
     """Base chrtme library class"""
 
-    def __init__(self, url, location, cmd):
-        self.__url__ = url
-        self.__location__ = location
-        self.__cmd__ = cmd
+    def _validateURL(self,url):
+        """Validate URLs used to download images"""
+        from urllib.parse import urlparse
+        parsed_url = urlparse(url)
+        allowed_schemes = [
+            'http', 'https',
+            'file'
+        ]
+        if ( 
+            not parsed_url.scheme or 
+            not parsed_url.scheme in allowed_schemes
+        ):
+            raise ValueError("Invalid URL scheme: http, https and file only.")
+        return parsed_url.geturl()
+
+    def _validateLocation(self,location):
+        """Validate target chrootfs location"""
+        import os
+        if not ( 
+            os.path.exists(location) or
+            os.path.exists( # location parent directory
+                os.path.abspath(
+                    os.path.join(location,os.path.pardir)
+                )
+            )
+        ):
+            raise ValueError("Specified location and parent directory don't exist")
+
+    def __init__(self, url, cmd, location=None):
+        # URL
+        try:
+            self._url = self._validateURL(url)
+        except:
+            raise
+
+        # CMD
+        self._cmd = cmd
+
+        # Location
+        if not location:
+            import os
+            location = os.path.join(os.getcwd(),'chrootfs')
+
+        try:
+            self._location = self._validateLocation(location)
+        except:
+            raise
--- tests/test_chrtme.py
+++ tests/test_chrtme.py
@@ -11,17 +11,31 @@ class ChrtmeTestToolkit(unittest.TestCase):
     """Base class for all Chrtme tests."""
 
     @classmethod
+    def _getWorkingVal(cls,var_name):
+        """Easily get values that are known to work"""
+        from os import getcwd
+        if var_name == 'url':
+            return 'http://working.URL'
+        elif var_name == 'location':
+            return getcwd()
+
+    @classmethod
     def setUp(cls):
         """Pre-test setup steps"""
         from .context import chrtme
         cls.chrtme_lib = chrtme
-        cls.chrtme_local_obj = cls.chrtme_lib.Chrtme(url='',location='',cmd='')
+        test_url = cls._getWorkingVal('url')
+        cmd = ''
+        cls.chrtme_obj = cls.chrtme_lib.Chrtme(
+            url=test_url,
+            cmd=cmd
+        )
 
     @classmethod
     def tearDown(cls):
         """Post-test cleaning steps"""
         del cls.chrtme_lib
-        del cls.chrtme_local_obj
+        del cls.chrtme_obj
 
 
 class ChrtmeTest(ChrtmeTestToolkit):
@@ -40,7 +54,12 @@ class ChrtmeTest(ChrtmeTestToolkit):
     def testObject(self):
         """Testing creating chrtme object"""
         try:
-            chrtme_local_obj = self.chrtme_lib.Chrtme(url='',location='',cmd='')
+            test_url = self._getWorkingVal('url')
+            cmd = ''
+            chrtme_local_obj = self.chrtme_lib.Chrtme(
+                url=test_url,
+                cmd=cmd,
+            )
         except:
             self.fail('Error during chrtme object creation')
         self.assertIsInstance(chrtme_local_obj, self.chrtme_lib.Chrtme)
@@ -48,14 +67,40 @@ class ChrtmeTest(ChrtmeTestToolkit):
     def testObjHasDownloadURL(self):
         """Testing Object URL attribute existence"""
         with self.assertRaises(TypeError, msg="URL not provided") as e:
-            new_chrtme_obj = self.chrtme_lib.Chrtme()
+            cmd = ''
+            chrtme_local_obj = self.chrtme_lib.Chrtme(
+                cmd=cmd
+            )
 
     def testObjURLCorrectlySet(self):
         """Test if chrtme object URL correct set"""
-        test_url = 'fake_URL'
-        new_chrtme_obj = self.chrtme_lib.Chrtme(url=test_url, location='', cmd='')
-        self.assertEqual(new_chrtme_obj.__url__, test_url)
+        test_url = self._getWorkingVal('url')
+        cmd = ''
+        chrtme_local_obj = self.chrtme_lib.Chrtme(
+            url=test_url,
+            cmd=cmd
+        )
+        self.assertEqual(chrtme_local_obj._url, test_url)
 
+    def testObjURLVerifiesValidity(self):
+        """Test if broken URLs are identified"""
+        cmd = ''
+        broken_url = 'not_http://i.should.not.be.working.com'
+        with self.assertRaises(ValueError, msg="Invalid URL not caught") as e:
+            chrtme_local_obj = self.chrtme_lib.Chrtme(
+                url=broken_url,
+                cmd=cmd,
+            )
+
+    def testObjURLValidityLocation(self):
+        """Test if the URL is correctly validated using file://"""
+        test_url = 'file://i.should.be.working'
+        cmd = ''
+        chrtme_local_obj = self.chrtme_lib.Chrtme(
+            url=test_url,
+            cmd=cmd
+        )
+        self.assertEqual(chrtme_local_obj._url, test_url)
 
 if __name__ == '__main__':
     unitttest.main()
